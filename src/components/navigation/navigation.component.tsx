058785fe4acdfff25906a4e3f1e889e3d784d8b1
Jacob Luckie <jakeluckie@gmail.com>
2024-06-19 00:16:35 +1200

    home page building

--- src/components/navigation/navigation.component.tsx
+++ src/components/navigation/navigation.component.tsx
@@ -1,16 +1,30 @@
 import { Link } from 'react-router-dom';
-import netcraftLogo from "../../assets/netcraft-logo.png";
+import netcraftLogo from "../../assets/netcraft-logo.svg";
+
+import './navigation.styles.scss';
 
 const Navigation = () => {
   return (
-    <div className='navigation'>
+    <header className='navigation'>
       <Link className='nc-logo' to={'/'}><img src={netcraftLogo} /></Link>
-      <ul>
+      <ul className='nav-links-container'>
         <li>
-          
+          VOIP Systems
+        </li>
+        <li>
+          Training
+        </li>
+        <li>
+          Internet Connections
+        </li>
+        <li>
+          About
+        </li>
+        <li>
+          Contact
         </li>
       </ul>
-    </div>
+    </header>
   )
 }
 
--- src/routes/home/home.component.tsx
+++ src/routes/home/home.component.tsx
@@ -1,11 +1,15 @@
 import Navigation from "../../components/navigation/navigation.component"
+import './home.styles.scss';
 
 const Home = () => {
   document.title = 'Netcraft - Bleeble'
   return (
     <>
+    <section className='home-container'>
     <Navigation />
       <h1>Netcraft - Homepage</h1>
+    </section>
+
     </>
   );
 };
