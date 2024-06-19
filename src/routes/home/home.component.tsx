c77ed23408123778a5869a64ddcb92265c377443
Jake <jakeluckie@gmail.com>
2024-06-19 16:24:01 +1200

    homepage work

--- src/routes/home/home.component.tsx
+++ src/routes/home/home.component.tsx
@@ -1,15 +1,24 @@
-import Navigation from "../../components/navigation/navigation.component"
-import './home.styles.scss';
+import Navigation from "../../components/navigation/navigation.component";
+import "./home.styles.scss";
 
 const Home = () => {
-  document.title = 'Netcraft - Bleeble'
+  document.title = "Netcraft - Bleeble";
   return (
     <>
-    <section className='home-container'>
-    <Navigation />
-      <h1>Netcraft - Homepage</h1>
-    </section>
-
+      <section className="home-container">
+        <Navigation />
+        <div className="content-padding">
+          <div>
+          <h1 className="hero-h1">VOIP Phone Systems</h1>
+          <p className="hero-text">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime
+            eligendi minima deleniti in ea dolores laudantium dolorum, assumenda
+            perspiciatis dolore vero fugit recusandae?
+          </p>
+          </div>
+          <div></div>
+        </div>
+      </section>
     </>
   );
 };
