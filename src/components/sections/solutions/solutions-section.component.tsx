024515caab626088454596353cc2e3d236b4d09b
Jacob Luckie <jakeluckie@gmail.com>
2024-06-30 22:35:28 +1200

    solutions section

--- src/components/sections/solutions/solutions-section.component.tsx
+++ src/components/sections/solutions/solutions-section.component.tsx
@@ -3,25 +3,25 @@ import "../../sections/solutions/solutions-section.styles.scss";
 
 const solutionsCardInfo: { img: string; title: string; description: string }[] = [
   {
-    img: "/solutions-images/netcraft-voip-phone-sm.webp",
+    img: "/solutions-images/nc-voip-systems-card-img.webp",
     title: "VOIP Systems",
     description:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti provident aperiam excepturi et a nulla."
   },
   {
-    img: "/solutions-images/netcraft-voip-phone-sm.webp",
+    img: "/solutions-images/nc-voip-systems-card-img.webp",
     title: "Internet Connections",
     description:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti provident aperiam excepturi et a nulla."
   },
   {
-    img: "/solutions-images/netcraft-voip-phone-sm.webp",
+    img: "/solutions-images/nc-voip-systems-card-img.webp",
     title: "Training",
     description:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti provident aperiam excepturi et a nulla."
   },
   {
-    img: "/solutions-images/netcraft-voip-phone-sm.webp",
+    img: "/solutions-images/nc-voip-systems-card-img.webp",
     title: "Consultancy",
     description:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti provident aperiam excepturi et a nulla."
@@ -33,8 +33,8 @@ const SolutionsSection = () => {
     <section className="solutions-container">
       <div className="content-padding">
         <div className="solutions-section-intro">
-          <h1>Our Services</h1>
-          <p>
+          <h1 className="solutions-section-title">Our Services</h1>
+          <p className="solutions-section-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
             minus autem, dolor veritatis, delectus ducimus, velit sunt maiores
             fugit eaque fugiat esse mollitia accusantium. Minus labore assumenda
